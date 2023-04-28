8492990ac8a64ecbf267d4a77431fb795cd6551c
ony <ogbuebele.ony@hotmail.com>
2023-04-28 14:18:14 +0200

    Add function to download image

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 import { createWriteStream } from 'node:fs';
-import { get } from 'node:https';
 import { mkdir } from 'node:fs/promises';
+import { get } from 'node:https';
+import cheerio from 'cheerio';
+
+async function imageDownload(url, path) {
+  const file = createWriteStream(path);
+  const response = await new Promise((resolve, reject) => {
+    get(url, resolve).on('error', reject);
+  });
+  response.pipe(file);
+  console.log('Image downloaded: ' + path);
+}
+
+imageDownload(
+  'https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg?width=300',
+  './memes/01.jpg',
+);
